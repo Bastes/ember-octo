077c3fd84595bc449e0a9cecaedbc7ff23f1db6e
Michel Belleville <mib@octo.com>
2013-02-01 19:18:11 +0100

    Adding helpers for readability

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -22,9 +22,25 @@
   App.Post = DS.Model.extend({
     title:      DS.attr('string'),
     body:       DS.attr('string'),
-    createdAt:  DS.attr('string'),
+    createdAt:  DS.attr('date'),
 
     comments:   DS.hasMany('App.Comment'),
     categories: DS.hasMany('App.Category')
+  });
+
+  Ember.Handlebars.registerBoundHelper('dateFormat', function (date) {
+    var day = date.getDate(),
+        month = date.getMonth() + 1,
+        year = date.getFullYear(),
+        zeroPad = function (value) { return (value < 10 ? '0' : '') + value };
+    return year + '-' + zeroPad(month) + '-' + zeroPad(day);
+  });
+
+  Ember.Handlebars.registerBoundHelper('paragraphed', function (string) {
+    return new Handlebars.SafeString(string.
+      replace(/^\s*/, '<p>').
+      replace(/\s*$/, '</p>').
+      replace(/\n{2,}/g, "</p><p>").
+      replace(/\n/g, '<br/>'));
   });
 })();
