919a60c9d1a01eb40eafdd2000636aa9269b00f3
Michel Belleville <mib@octo.com>
2013-02-01 13:41:59 +0100

    Database, models and get routes

--- application.rb
+++ application.rb
@@ -24,7 +24,87 @@ class Application < Sinatra::Base
     css_compression :sass
   end
 
+  configure :development do
+    DataMapper::Logger.new($stdout, :debug)
+    DataMapper.setup :default, "sqlite://#{File.expand_path(File.join(__FILE__, '..', 'db.sqlite'))}"
+  end
+
+  class Post
+    include DataMapper::Resource
+
+    property :id,         Serial
+    property :title,      String
+    property :body,       Text
+    property :created_at, DateTime
+
+    has n, :comments
+    has n, :categorizations
+    has n, :categories, through: :categorizations
+  end
+
+  class Comment
+    include DataMapper::Resource
+
+    property :id,         Serial
+    property :posted_by,  String
+    property :body,       Text
+    property :created_at, DateTime
+
+    belongs_to :post
+  end
+
+  class Category
+    include DataMapper::Resource
+
+    property :id,   Serial
+    property :name, String
+
+    has n, :categorizations
+    has n, :posts, through: :categorizations
+  end
+
+  class Categorization
+    include DataMapper::Resource
+
+    property :id, Serial
+
+    belongs_to :category
+    belongs_to :post
+  end
+
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+
   get '/' do
     erb :index
+  end
+
+  get '/posts' do
+    content_type :json
+    Post.all.to_json
+  end
+
+  get '/posts/:id' do |id|
+    content_type :json
+    Post.get!(id).to_json
+  end
+
+  post '/posts' do
+    render Post.create(params[:post]).to_json
+  end
+
+  get '/posts/:id/comments' do |id|
+    content_type :json
+    Post.get!(id).comments.to_json
+  end
+
+  get '/categories' do
+    content_type :json
+    Category.all.to_json
+  end
+
+  get '/categories/:id/posts' do |id|
+    content_type :json
+    Category.get!(id).posts.to_json
   end
 end
