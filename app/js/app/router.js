79789a71b4469b2f34c7f39de6fcc0823cb19b13
Michel Belleville <mib@octo.com>
2013-02-15 19:39:55 +0100

    Blank slate for the embering

--- app/js/app/router.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  window.App.Router.map(function () {
-    this.resource('posts', function () {
-      this.route('new');
-      this.route('show', {path: '/:post_id'});
-      this.route('edit', {path: '/:post_id/edit'});
-    });
-  });
-})();
