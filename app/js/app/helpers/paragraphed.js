f87946643ffc487c8b882de5e95db9abbb1f9edb
Michel Belleville <mib@octo.com>
2013-03-15 12:07:18 +0100

    Removing paragraph treatment (preparing for dojo)

--- app/js/app/helpers/paragraphed.js
+++
@@ -1,8 +0,0 @@
-Ember.Handlebars.registerBoundHelper('paragraphed', function (string) {
-  if (!string) return '';
-  return new Handlebars.SafeString(string.
-    replace(/^\s*/, '<p>').
-    replace(/\s*$/, '</p>').
-    replace(/\n{2,}/g, "</p><p>").
-    replace(/\n/g, '<br/>'));
-});
