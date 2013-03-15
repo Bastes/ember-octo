406835155eb06e6cfc04d380f5ccb67dbbaf462e
Michel Belleville <mib@octo.com>
2013-03-15 12:07:43 +0100

    Removing after save hook (preparing for dojo)

--- app/js/app/controllers/posts/new.js
+++ app/js/app/controllers/posts/new.js
@@ -1,14 +1,10 @@
 App.PostsNewController = Ember.ObjectController.extend({
   save: function () {
     this.store.commit();
-    this.content.addObserver('id', this, 'afterSave');
+    this.transitionToRoute('post', this.content);
   },
   cancel: function () {
     this.content.deleteRecord();
     this.transitionToRoute('posts.index');
-  },
-  afterSave: function () {
-    this.content.removeObserver('id', this, 'afterSave');
-    this.transitionToRoute('post', this.content);
   }
 });
