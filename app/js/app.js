b2e9b0e759b40aee589fe356a3770e2c4b8fcb9c
Jeremy Venezia <jvenezia@octo.com>
2013-04-18 10:48:10 +0200

    added table model and fixtures

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
-App = Ember.Application.create()
+window.App = Ember.Application.create();
+
+App.Store = DS.Store.extend({
+  revision: 11,
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Table = DS.Model.extend({
+	name: DS.attr("string")
+});
+
+App.Table.FIXTURES = [
+  {id: 1, name: "Table 1"},
+  {id: 2, name: "Table 2"},
+  {id: 3, name: "Table 3"},
+  {id: 4, name: "Table 4"},
+  {id: 5, name: "Table 5"},
+  {id: 6, name: "Table 6"}
+];
