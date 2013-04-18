092b81242114453b5b653df45292bc90af84a83c
Jeremy Venezia <jvenezia@octo.com>
2013-04-18 12:14:38 +0200

    formatPrice refactor

--- app/js/app.js
+++ app/js/app.js
@@ -90,9 +90,7 @@ App.Food.FIXTURES = [
 ];
 
 Ember.Handlebars.registerBoundHelper('formatPrice', function(value) {
-	if (isNaN(value)) {
-		return "0.00 €";
-	}
+	value = Number(value);
 	value = value*100;
 	price = (value % 100 === 0 ? value / 100 + ".00" : parseInt(value / 100, 10) + "." + value % 100);
 	return price + ' €';
